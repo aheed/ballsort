0e3ce23e3f7599f27945ebf0c9d3f54226598985
Anders Heed <aheed@users.noreply.github.com>
2023-10-13 20:29:17 +0200

    Multiple claws (#5)
    * move css to separate file
    
    * move backdrop into separate file
    
    * put constants in separate file
    
    * add ball.js
    
    * put claw related variables in a single object
    
    * add claw.js
    
    * handle claw array in received message
    
    * cleanup

---
+++ backdrop.js
@@ -0,0 +1,78 @@
+import {hDistance, vDistance, vTopSpace} from "./constants.js";
+
+const _createHighlight = ({ xMin, xMax, yMin, yMax, color }) => {
+  const backDrop = document.querySelector("#backdrop");
+
+  for (let xIndex = xMin; xIndex <= xMax; xIndex++) {
+    const rectLeftSpace = 50;
+    const x = rectLeftSpace + (xIndex - 0.5) * hDistance;
+
+    for (let yIndex = yMin; yIndex <= yMax; yIndex++) {
+      const rectTopSpace = vTopSpace + 51;
+      const y = rectTopSpace + yIndex * vDistance;
+      let rectangle = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "rect"
+      );
+      rectangle.setAttribute("x", `${x}`);
+      rectangle.setAttribute("y", `${y}`);
+      rectangle.setAttribute("width", `${hDistance - 1}`);
+      rectangle.setAttribute("height", `${vDistance - 1}`);
+      rectangle.setAttribute("fill", color);
+      backDrop.appendChild(rectangle);
+    }
+  }
+};
+
+const _createGridLabels = (maxX, maxY) => {
+  const backDrop = document.querySelector("#backdrop");
+
+  for (let xIndex = 0; xIndex <= maxX; xIndex++) {
+    const rectLeftSpace = 50;
+    const x = rectLeftSpace + (xIndex - 0.5) * hDistance;
+    const y = vTopSpace + 52 + vDistance * maxY;
+
+    var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    text.setAttribute("x", `${x + 22}`);
+    text.setAttribute("y", `${y + 59}`);
+    text.textContent = `${xIndex}`;
+    text.setAttribute("font-size", "24");
+    backDrop.appendChild(text);
+  }
+
+  for (let yIndex = 0; yIndex <= maxY; yIndex++) {
+    const rectLeftSpace = 20;
+    const x = rectLeftSpace;
+    const rectTopSpace = vTopSpace + 51;
+    const y = rectTopSpace + yIndex * vDistance;
+
+    var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    text.setAttribute("x", `${x - 20}`);
+    text.setAttribute("y", `${y + 25}`);
+    text.textContent = `${yIndex}`;
+    text.setAttribute("font-size", "24");
+
+    backDrop.appendChild(text);
+  }
+};
+
+export const recreateBackdrop = (maxX, maxY, highlights) => {
+  const backDrop = document.querySelector("#backdrop");
+
+  while (backDrop.firstChild) {
+    backDrop.removeChild(backDrop.firstChild);
+  }
+
+  _createHighlight({
+    xMin: 0,
+    xMax: maxX,
+    yMin: 0,
+    yMax: maxY,
+    color: "lightgray",
+  });
+
+  highlights.forEach((highlight) => {
+    _createHighlight(highlight);
+  });
+  _createGridLabels(maxX, maxY);
+};
---
+++ constants.js
@@ -0,0 +1,5 @@
+export const hDistance = 60;
+export const vTopSpace = 100;
+export const vDistance = 40;
+export const originX = 50;
+export const originY = 70;
